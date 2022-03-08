49c14e2fee520ee4f37218bbbf82934e6287d6f1
neelptl246 <97786888+neelptl246@users.noreply.github.com>
2022-03-08 23:23:43 +0530

    Create ass9.py

---
+++ ass9.py
@@ -0,0 +1,28 @@
+from logging import root
+
+
+class student(object):
+    def __init__(self,name,rollNo):
+        self.name=name
+        self.rollno=rollNo
+# creating class exam
+
+class exam(student):
+    def __init__(self,name,rollNo,marks):
+        super().__init__(name,rollNo)
+        self.marks=marks
+        
+#creating class result  
+
+class result(exam):
+    def __init__(self, name,rollNo,marks):
+        super().__init__(name,rollNo,marks)
+        self.resultt=marks
+        self.total_marks=self.resultt/600*100
+        self.percentage=self.total_marks
+     
+    def display(self):
+        return self.percentage
+
+s=result("Suman",1,500)
+print(s.display())
